3531a2447d3cbddbd1c56d32ac85f69b08fbcdf7
Juan <juanci123z@gmail.com>
2025-09-16 01:45:21 -0500

    arreglando el comando

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import {Client} from 'discord.js';
 import { config } from './config';
 import { commands } from './commands';
 import { deployCommands } from './deploy-commands';
+import { Events } from 'discord.js';
 
 const client = new Client({
     intents:["Guilds", "GuildMessages", "DirectMessages"],
@@ -13,6 +14,14 @@ client.once("ready", ()=>{
 
 client.on("guildCreate", async (guild)=>{
     await deployCommands({guildId: guild.id});
+})
+
+client.once(Events.ClientReady, async (c)=>{
+    console.log (`✅ Ya está listo el bot: ${c.user.tag}`);
+
+    for (const [guildId] of c.guilds.cache){
+        await deployCommands({guildId});
+    }
 })
 
 client.on("interactionCreate", async (interaction) =>{
