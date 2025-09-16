c9b04583a33bbca58920526eadf9d6b06492d162
Juan <juanci123z@gmail.com>
2025-09-16 01:49:09 -0500

    arreglando el comando

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -4,6 +4,6 @@ export const data = new SlashCommandBuilder()
 .setName("help")
 .setDescription("Lista todos los comandos disponibles");
 
-export async function execute(interaction: CommandInteraction,){
+export async function execute(interaction: CommandInteraction){
     await interaction.reply("Comandos disponibles: `/ping`, `/help`");
 }
